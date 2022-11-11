d9c0ef56656c1ac6c5444d5206ef6441580751da
balazsmolnar87 <balazs.millair@gmail.com>
2022-11-11 17:36:47 +0100

    Add basic app

---
+++ app.py
@@ -0,0 +1,19 @@
+﻿from flask import Flask
+
+app = Flask(__name__)
+
+
+def sum_digits(number: str):
+    sum_of_digits = 0
+    for digit in number:
+        sum_of_digits += int(digit)
+    return sum_of_digits
+
+
+@app.route('/sum-of-digits/<number>')
+def sum_request(number: str):
+    return sum_digits(number)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ test.py
@@ -0,0 +1,17 @@
+﻿from app import sum_digits
+
+
+def test_sum_digits_no_entry():
+    assert sum_digits() == 0
+
+
+def test_sum_digits_one_digit():
+    assert sum_digits(1) == 1
+
+
+def test_sum_digits_two_digits():
+    assert sum_digits(12) == 3
+
+
+def test_sum_digits_three_digits():
+    assert sum_digits(123) == 6
